fdd584c9a221be78453e04f0693d9d0b554bd6dd
xStarH4ndx <brunotoro1799@gmail.com>
2025-03-31 15:33:38 -0300

    resolver de asignatura corregido

--- proyecto/src/main/java/com/arquitectura/proyecto/resolver/AsignaturaResolver.java
+++ proyecto/src/main/java/com/arquitectura/proyecto/resolver/AsignaturaResolver.java
@@ -2,20 +2,26 @@ package com.arquitectura.proyecto.resolver;
 
 import com.arquitectura.proyecto.model.Asignatura;
 import com.arquitectura.proyecto.service.AsignaturaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@Component
+@Controller
 public class AsignaturaResolver {
 
-    @Autowired
-    private AsignaturaService asignaturaService;
+    private final AsignaturaService asignaturaService;
+
+    public AsignaturaResolver(AsignaturaService asignaturaService) {
+        this.asignaturaService = asignaturaService;
+    }
 
     @QueryMapping
     public List<Asignatura> obtenerTodasAsignaturas() {
-        return asignaturaService.obtenerTodasAsignaturas();
+        try{
+            return asignaturaService.obtenerTodasAsignaturas();
+        }catch (Exception e){
+            throw new RuntimeException("Error al obtener las asignaturas", e);
+        }
     }
 }
--- proyecto/src/main/java/com/arquitectura/proyecto/service/AsignaturaService.java
+++ proyecto/src/main/java/com/arquitectura/proyecto/service/AsignaturaService.java
@@ -14,6 +14,8 @@ public class AsignaturaService {
     private AsignaturaRepository asignaturaRepository;
 
     public List<Asignatura> obtenerTodasAsignaturas() {
-        return asignaturaRepository.findAll();
+        List<Asignatura> asignaturas = asignaturaRepository.findAll();
+        System.out.println("Asignaturas----------------------------: " + asignaturas);
+        return asignaturas;
     }
 }
